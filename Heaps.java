d359d9fd4f7e65853fc79587971f0b6da9b888fc
unknown <shivamsomani9561@gmail.com>
2024-04-16 00:02:23 +0530

    File Added

---
+++ Heaps.java
@@ -0,0 +1,27 @@
+public class Heaps {
+
+    public static void createHeap(int index,int[] array)
+    {
+        
+    }
+
+    public static swap()
+    {
+
+    }
+    public static void main(String[] args) {
+        int[] array={10,5,6,2,3,1};
+
+        for(int i=0;i<array.length;i++)
+        {
+            System.out.println(array[i]+ " ");
+        }
+
+        int index=0;
+        while(index<array.length)
+        {
+            createHeap(index,array);
+            index++;
+        }
+    }
+}
